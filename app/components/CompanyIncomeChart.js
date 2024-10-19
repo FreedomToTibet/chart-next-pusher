2607ea3f0fa5dba228617800ad8ac8df3974561b
FreedomToTibet <98056258+FreedomToTibet@users.noreply.github.com>
2024-10-19 05:02:35 -0700

    Chart component

--- app/components/CompanyIncomeChart.js
+++ app/components/CompanyIncomeChart.js
@@ -3,7 +3,7 @@
 import { useEffect, useRef, useState } from 'react';
 import axios from 'axios';
 import ChartButton from '@/app/components/ChartButton';
-// import Toggle3DButton from '@/app/components/Toggle3DButton';
+import Toggle3DButton from '@/app/components/Toggle3DButton';
 
 export default function CompanyIncome() {
 	const initialData = [
@@ -17,71 +17,123 @@ export default function CompanyIncome() {
 
 	const chartRef = useRef(null);
 	const [chartData, setChartData] = useState([...initialData]);
-	// const [toggle3D, setToggle3D] = useState(false);
+	const [toggle3D, setToggle3D] = useState(false);
 
 	useEffect(() => {
-		// Dynamically import Highcharts and its 3D module
-		import('highcharts/highcharts-3d').then((Highcharts3D) => {
-			import('highcharts').then((Highcharts) => {
-				Highcharts3D.default(Highcharts.default);
+    // Dynamically import Highcharts and its 3D module
+    import('highcharts/highcharts-3d').then((Highcharts3D) => {
+      import('highcharts').then((Highcharts) => {
+        Highcharts3D.default(Highcharts.default);
 
-				chartRef.current = Highcharts.default.chart('chart-container', {
-					colors: ['#F3F7FB', '#F3F7FB'],
-					chart: {
-						style: {
-							fontFamily: ['Prompt', 'sans-serif'],
-							fontSize: '16px',
-						},
-						type: 'column',
-						// options3d: {
-						//   enabled: toggle3D,
-						//   alpha: 10,
-						//   beta: 25,
-						//   depth: 70,
-						//   viewDistance: 25,
-						// },
-						backgroundColor: {
-							linearGradient: [0, 0, 500, 500],
-							stops: [
-								[0, 'rgb(128, 130, 221)'],
-								[1, 'rgb(128, 130, 221)'],
-							],
-						},
-					},
-					title: {
-						text: 'COMPANY ANNUAL INCOME',
-						style: {
-							fontSize: '27px',
-						},
-					},
-					xAxis: {
-						title: {
-							text: 'Year',
-							style: {
-								fontSize: '18px',
-							},
-						},
-						categories: chartData.map((data) => data[0]),
-					},
-					yAxis: {
-						title: {
-							text: 'Income',
-							style: {
-								fontSize: '18px',
-							},
-						},
-					},
-					series: [
-						{
-							name: 'Income',
-							data: chartData.map((data) => data[1]),
-						},
-					],
-				});
-			});
-		});
-	}, [chartData]);
+        chartRef.current = Highcharts.default.chart('chart-container', {
+          colors: ['#F3F7FB', '#F3F7FB'],
+          chart: {
+            style: {
+              fontFamily: ['Prompt', 'sans-serif'],
+              fontSize: '16px',
+            },
+            type: 'column',
+            options3d: {
+              enabled: toggle3D,
+              alpha: 10,
+              beta: 25,
+              depth: 70,
+              viewDistance: 25,
+            },
+            backgroundColor: {
+              linearGradient: [0, 0, 500, 500],
+              stops: [
+                [0, 'rgb(128, 130, 221)'],
+                [1, 'rgb(128, 130, 221)'],
+              ],
+            },
+          },
+          title: {
+            text: 'ANNUAL INCOME',
+            style: {
+              fontSize: '27px',
+            },
+          },
+          xAxis: {
+            title: {
+              text: 'Year',
+              style: {
+                color: 'white',
+              },
+            },
+            labels: {
+              style: {
+                color: 'white',
+              },
+            },
+            type: 'category',
+          },
+          yAxis: {
+            title: {
+              text: 'Income',
+              style: {
+                color: 'white',
+              },
+            },
+            labels: {
+              style: {
+                color: 'white',
+              },
+            },
+            min: 0,
+          },
+          credits: {
+            text: '',
+          },
+          series: [
+            {
+              name: 'Income',
+              data: chartData,
+            },
+          ],
+          animation: {
+            duration: 100,
+          },
+        });
+      });
+    });
 
-	useEffect(() => {
-		axios.get('/
+		const interval = setInterval(async () => {
+      try {
+        const response = await axios.get('/api/pusher');
+        const newDataPoint = [
+          chartData[chartData.length - 1][0] + 1,
+          response.data.value,
+        ];
+        chartRef.current.series[0].addPoint(newDataPoint, true, true);
+        setChartData((prevData) => [...prevData, newDataPoint]);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+      // the setInterval is the same as API setInterval
+    }, 10000);
+    return () => {
+      clearInterval(interval);
+    };
+  }, [toggle3D]);
+
+	return (
+		<div>
+      <div id="chart-container" className="w-full h-96"></div>
+      <div className="border-solid border-2 border-indigo-500 m-10">
+        <div className="grid justify-center mt-5 text-center">
+          <p>3D Mode</p>
+          <div className="w-20">
+            <Toggle3DButton toggle3D={toggle3D} setToggle3D={setToggle3D} />
+          </div>
+        </div>
+        <div className="grid gap-4 justify-center mt-5 xl:grid-cols-3 p-4">
+          <ChartButton chartRef={chartRef} type={'area'} name={'Area Chart'} />
+          <ChartButton chartRef={chartRef} type={'bar'} name={'Bar Chart'} />
+          <ChartButton chartRef={chartRef} type={'line'} name={'Line Chart'} />
+        </div>
+      </div>
+    </div>
+	)
+
 }
